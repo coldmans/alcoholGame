1a3801e780c949be89b0d86f2c8355fd7f5af8b6
jinhyeong jang <jinhinjala26@naver.com>
2025-07-11 21:11:44 +0900

    init

---
+++ src/main/java/com/example/alcoholGameBackend/AlcoholGameBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.alcoholGameBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlcoholGameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlcoholGameBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/alcoholGameBackend/AlcoholGameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alcoholGameBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlcoholGameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
